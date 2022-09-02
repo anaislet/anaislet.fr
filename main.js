b70f749d417754ad43d7b68d6ca42f56ccf0bebf
anaislet <anais.laura.letellier@gmail.com>
2022-09-02 19:20:07 +0200

    Initialisation du site anaislet.fr

---
+++ main.js
@@ -0,0 +1,79 @@
+const mapbox = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+        tileSize: [256, 256],
+        url: 'https://api.mapbox.com/styles/v1/adrienvh/cl7923v83000m14mrmri65tyj/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWRyaWVudmgiLCJhIjoiU2lDV0N5cyJ9.2pFJAwvwZ9eBKKPiOrNWEw'
+    })
+})
+
+const epitechSource = new ol.source.Vector({ projection: 'EPSG:3857' })
+const feature = new ol.Feature({})
+feature.setGeometry(new ol.geom.Point(ol.proj.fromLonLat([2.3631677114721135, 48.81547547529164])))
+epitechSource.addFeature(feature)
+const epitech = new ol.layer.Vector({
+    source: epitechSource,
+    style: new ol.style.Style({
+        image: new ol.style.Circle({
+            stroke: new ol.style.Stroke({color: 'white', width: 2}),
+            fill: new ol.style.Fill({color: '#7a7d68'}),
+            radius: 8
+        })
+    })
+})
+
+var carte = new ol.Map({
+    interactions: ol.interaction.defaults({
+        doubleClickZoom: false,
+        dragAndDrop: false,
+        dragPan: false,
+        keyboardPan: false,
+        keyboardZoom: false,
+        mouseWheelZoom: false,
+        pointer: false,
+        select: false
+    }),
+    controls: ol.control.defaults({
+        attribution: false,
+        zoom: false,
+        rotate: false
+    }),
+    target: 'carte',
+    layers: [mapbox, epitech],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([2.338060305360853, 48.83602051737811]),
+      zoom: 12
+    })
+});
+
+
+const mapbox2 = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+        tileSize: [256, 256],
+        url: 'https://api.mapbox.com/styles/v1/adrienvh/cl7ezt1ty000914o7i7nfkfr8/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWRyaWVudmgiLCJhIjoiU2lDV0N5cyJ9.2pFJAwvwZ9eBKKPiOrNWEw'
+    })
+})
+
+var carte2 = new ol.Map({
+    interactions: ol.interaction.defaults({
+        doubleClickZoom: false,
+        dragAndDrop: false,
+        dragPan: false,
+        keyboardPan: false,
+        keyboardZoom: false,
+        mouseWheelZoom: false,
+        pointer: false,
+        select: false,
+    }),
+    controls: ol.control.defaults({
+        attribution: false,
+        zoom: false,
+        rotate: false
+    }),
+    target: 'senlis',
+    layers: [mapbox2],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([2.5833153186184448, 49.20639599000746]),
+      zoom: 16
+    })
+});
+
+document.getElementById("retour").addEventListener("click", function() {window.scrollTo(0, 0);});
